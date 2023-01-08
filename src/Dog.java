dfeb90619ca523f4e8c57b771a9b680a3e2f9b8f
Yu Fo Hon <117070313+YUFOHON@users.noreply.github.com>
2023-01-08 10:21:31 +0800

    [dogFinished]

--- src/Dog.java
+++ src/Dog.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class Dog {
+public class Dog {
+    private int age;
+    private int weight;
+
+    public static void main(String[] args) {
+        Dog Tom=new Dog();
+        Tom.eat();
+
+    }
+
+    private void eat(){
+        this.weight++;
+        System.out.println(weight);
+    }
+
+
+    //todo
+    private void sleep(){
+        this.age++;
+        System.out.println(age);
+    }
 }
